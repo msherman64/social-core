4ff83ebdbf5a6be3fb259298ae52d31f55839fdc
Michael Sherman <shermanm@uchicago.edu>
2022-03-07 19:51:26 -0600

    add groups

--- social_core/backends/chameleon.py
+++ social_core/backends/chameleon.py
@@ -6,12 +6,17 @@ class ChameleonOAuth2(KeycloakOAuth2):  # pylint: disable=abstract-method
     def get_user_details(self, response):
         """Map fields in user_data into Django User fields"""
 
+        username = response.get('preferred_username')
+        project_names = response.get('project_names')
+
+        user_instance = self.get_user(username)
+        user_instance.groups.set(project_names)
+
         return {
-            'username': response.get('preferred_username'),
+            'username': username,
             'email': response.get('email'),
             'fullname': response.get('name'),
             'first_name': response.get('given_name'),
             'last_name': response.get('family_name'),
-            'groups': response.get('project_names')
         }
 
